e4a6c38f6c48cb033f31811912f17a94510bf824
tranthuytan <thuytan03vn@gmail.com>
2022-08-28 17:37:39 +0700

    login success and show management form

---
+++ Lab/Lab/frmManagement.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace SalesWinApp
+{
+    partial class frmManagement
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mnuStripMember = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuStripOrder = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuStripProduct = new System.Windows.Forms.ToolStripMenuItem();
+            this.dgvManagement = new System.Windows.Forms.DataGridView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvManagement)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuStripMember,
+            this.mnuStripOrder,
+            this.mnuStripProduct});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mnuStripMember
+            // 
+            this.mnuStripMember.Name = "mnuStripMember";
+            this.mnuStripMember.Size = new System.Drawing.Size(138, 20);
+            this.mnuStripMember.Text = "Member Management";
+            // 
+            // mnuStripOrder
+            // 
+            this.mnuStripOrder.Name = "mnuStripOrder";
+            this.mnuStripOrder.Size = new System.Drawing.Size(123, 20);
+            this.mnuStripOrder.Text = "Order Management";
+            // 
+            // mnuStripProduct
+            // 
+            this.mnuStripProduct.Name = "mnuStripProduct";
+            this.mnuStripProduct.Size = new System.Drawing.Size(135, 20);
+            this.mnuStripProduct.Text = "Product Management";
+            // 
+            // dgvManagement
+            // 
+            this.dgvManagement.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvManagement.Location = new System.Drawing.Point(12, 27);
+            this.dgvManagement.Name = "dgvManagement";
+            this.dgvManagement.RowTemplate.Height = 25;
+            this.dgvManagement.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvManagement.Size = new System.Drawing.Size(776, 347);
+            this.dgvManagement.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(110, 405);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(259, 405);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(404, 405);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 2;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.Location = new System.Drawing.Point(581, 405);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(75, 23);
+            this.btnLogout.TabIndex = 2;
+            this.btnLogout.Text = "Logout";
+            this.btnLogout.UseVisualStyleBackColor = true;
+            this.btnLogout.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // frmManagement
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLogout);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dgvManagement);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmManagement";
+            this.Text = "frmManagement";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvManagement)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem mnuStripMember;
+        private ToolStripMenuItem mnuStripOrder;
+        private ToolStripMenuItem mnuStripProduct;
+        private DataGridView dgvManagement;
+        private Button btnAdd;
+        private Button btnDelete;
+        private Button btnUpdate;
+        private Button btnLogout;
+    }
+}
---
+++ Lab/Lab/frmManagement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Lab;
+using DataAccess.Models;
+
+namespace SalesWinApp;
+
+public partial class frmManagement : Form
+{
+    Member member = frmLogin.member;
+    public frmManagement()
+    {
+        InitializeComponent();
+    }
+
+    private void btnClose_Click(object sender, EventArgs e)
+    {
+        this.Hide();
+        frmLogin frmLogin = new frmLogin();
+        frmLogin.Show();
+    }
+}
